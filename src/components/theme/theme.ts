7e781abd499f2b2329815f1329ec65bb152fd6ce
Mbaocha Jonathan <mbaocha240793@gmail.com>
2023-01-31 12:33:14 -0800

    fixxed another bug

--- src/components/theme/theme.ts
+++ src/components/theme/theme.ts
@@ -1,22 +1,21 @@
 'use strict';
+import { createTheme } from '@mui/material/styles';
 
-import { createMuiTheme } from '@material-ui/core/styles';
-
-const theme = createMuiTheme({
-    overrides: {
-        // Applied to the <ul> element
-        MuiMenu: {
-            list: {
-                backgroundColor: "#cccccc",
-            },
-        },
-        // Applied to the <li> elements
-        MuiMenuItem: {
-            root: {
-                fontSize: 12,
-            },
-        },
-    },
+const theme = createTheme({
+    // overrides: {
+    //     // Applied to the <ul> element
+    //     MuiMenu: {
+    //         list: {
+    //             backgroundColor: "#cccccc",
+    //         },
+    //     },
+    //     // Applied to the <li> elements
+    //     MuiMenuItem: {
+    //         root: {
+    //             fontSize: 12,
+    //         },
+    //     },
+    // },
 });
 
 export default theme;
